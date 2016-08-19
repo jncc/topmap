e6f98669d62970f7f9b9131d47c6a1e5b99c9bd8
Matt Debont <matt.debont@jncc.gov.uk>
2016-08-19 12:03:32 +0100

    Working basic gulp example for serve

--- portal/gulpfile.js
+++ portal/gulpfile.js
@@ -11,60 +11,36 @@ var compass = require('gulp-compass')
 var plumber = require('gulp-plumber')
 var autoprefixer = require('gulp-autoprefixer')
 
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
+var foreach = require('gulp-foreach');
+var fs = require('fs');
+var path = require('path');
+var rework = require('gulp-rework');
+var reworkUrl = require('rework-plugin-url');
+var concat = require('gulp-concat');
+var gulpDebug = require('gulp-debug');
+
+var wiredep = require('wiredep').stream;
+
+var browserSync = require('browser-sync');
 
 var config = {
   bowerDir: './bower_components'
 }
 
+var bowerCopyFiles = [];
 
-gulp.task('default', function() {
-  // place code for your default task here
-  return gutil.log('Gulp Run')
-});
-
-gulp.task('coffee', function() {
+// Coffescript build to ./tmp
+gulp.task('scripts', function() {
   gulp.src('./app/**/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./.tmp/app'))
+    .pipe(gulp.dest('./.tmp'))
 });
 
-gulp.task('html_move', function() {
-  gulp.src('./app/**/*.html')
-    .pipe(gulp.dest('./.tmp/app'));
-});
-
-gulp.task('imagemin', function () {
-  gulp.src('./app/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./.tmp/app/images'))
-});
-
-gulp.task('sass', function() {
-  gulp.src('./app/styles/**/*.scss')
-    .pipe(sass({
-      loadPath: [
-        './app/styles',
-        config.bowerDir 
-      ]
-    }).on('error', gutil.log))
-    .pipe(gulp.dest('./.tmp/app/styles'))
-});
-
-gulp.task('compass', function() {
-  gulp.src('./app/styles/**/*.scss')
-    .pipe(compass({
-      css: './.tmp/app/styles',
-      sass: './.tmp/app/sass',
-      image: './.tmp/app/styles/images',
-      javascripts: './.tmp/app/scripts',
-      fonts: './.tmp/app/fonts',
-      includePaths: config.bowerDir
-    }))
-    .pipe(gulp.dest('./.tmp/app/styles'))
-});
-
+// Build SASS files
 gulp.task('styles', () => {
   return gulp.src('./app/styles/**/*.scss')
     .pipe(plumber())
@@ -77,8 +53,101 @@ gulp.task('styles', () => {
     .pipe(autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'));
-    .pipe(reload({stream: true}));
+    //.pipe(reload({stream: true}));
 });
+
+
+gulp.task('serve', ['styles', 'scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+});
+
+
+
+
+// Build-Time
+// gulp.task('imagemin', function () {
+//   gulp.src('./app/images/**/*')
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('./.tmp/app/images'))
+// });
+
+// gulp.task('html_move', function() {
+//   gulp.src('./app/**/*.html')
+//     .pipe(gulp.dest('./.tmp/app'));
+// });
+
+
+
+
+
+
+// gulp.task('bower', function() {
+//   gulp.src('./app/index.html')
+//     .pipe(wiredep())
+//     .pipe(gulpDebug())
+//     .pipe(gulp.dest('./.tmp/dest'));
+// });
+
+// gulp.task('build-dependencies', function() {
+//   console.log(mainBowerFiles("**/*.js"));
+//   return gulp.src(mainBowerFiles("**/*.js"))
+//     //.pipe(flatten())
+//     .pipe(gulp.dest('./.tmp/app/javascripts'));
+// });
+
+
+// //copy bower assets that need copying
+// gulp.task('bower:assets', function() {
+//     return gulp.src(mainBowerFiles(), {
+//         base: './bower_components'
+//     })
+//     .pipe(filter([
+//         '**/*.{png,gif,svg,jpeg,jpg,woff,eot,ttf}',
+//         '!foundation/**/*',
+//         '!compass-mixins/**/*'
+//     ]))
+//     .pipe(gulp.dest('./.tmp/app/styles/vendor'));
+// });
+
+// //generate bower stylesheets with correct asset paths
+// gulp.task('bower:styles', function() {
+//     return gulp.src(mainBowerFiles(), {
+//         base: './bower_components'
+//     })
+//     .pipe(filter([
+//         '**/*.{css,scss}',
+//         '!**/_bootstrap.scss',
+//         '!foundation/**/*',
+//         '!compass-mixins/**/*'
+//     ]))
+//     // .pipe(foreach(function(stream, file) {
+//     //     var dirName = path.dirname(file.path);
+//     //     return stream
+//     //         .pipe(rework(reworkUrl(function(url) {
+//     //             var fullUrl = path.join(dirName, url);
+//     //             if (fs.existsSync(fullUrl)) {
+//     //                 bowerCopyFiles.push(fullUrl);
+//     //                 console.log(path.relative('css', fullUrl).replace(/bower_components/, 'styles/vendor'));
+//     //                 return path.relative('css', fullUrl).replace(/bower_components/, 'styles/vendor');
+//     //             }
+//     //             return url;
+//     //         })));
+//     // }))
+//     .pipe(sourcemaps.init())
+//     //.pipe(concat('bower.css'))
+//     //.pipe(minifyCss())
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('./.tmp/app/styles/vendor'));
+// });
 
 
 gulp.task('clean', function() {
